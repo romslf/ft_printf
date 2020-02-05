cff8069c8e4880645b8ddafdd0b44285bcd0ecfc
romslf <rom.laforgue@gmail.com>
2020-02-05 18:18:37 +0100

    Progress

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rolaforg <rolaforg@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2020/02/04 15:55:34 by rolaforg     #+#   ##    ##    #+#       */
-/*   Updated: 2020/02/04 23:09:04 by rolaforg    ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/02/05 17:11:58 by rolaforg    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -136,6 +136,12 @@ void	ft_printf_hexa(va_list list)
 	ft_putnbr_base(num, "0123456789ABCDEF");
 }
 
+void	ft_print_address(va_list list)
+{
+	char **str = va_arg(list, char **);
+	ft_putstr(&str);
+}
+
 int		findIndex(char *flags, char element)
 {
 	int	i;
@@ -154,11 +160,12 @@ int		findIndex(char *flags, char element)
 
 int		ft_printf(const char *str, ...)
 {
-	void	(*functions[6]) (va_list) = {ft_print_str, ft_print_char, ft_print_nb, ft_print_nb, ft_printf_hex, ft_printf_hexa};
-	char	flags[7] = {'s', 'c', 'd', 'i', 'x', 'X',0};
+	void	(*functions[7]) (va_list) = {ft_print_str, ft_print_char, ft_print_nb, ft_print_nb, ft_printf_hex, ft_printf_hexa, ft_print_address};
+	char	flags[8] = {'s', 'c', 'd', 'i', 'x', 'X', 'p', 0};
 	va_list	list;
 	int		tmp;
 	int		i;
+	int		x = 0;
 
 	tmp = 0;
 	i = 0;
@@ -170,12 +177,12 @@ int		ft_printf(const char *str, ...)
 			tmp = findIndex(flags, str[i]);
 			if (tmp != -1)
 				(*functions[tmp]) (list);
+			x++;
 		} else if (str[i] != '%')
 			ft_putchar(str[i]);
 		i++;
 	}
-
-	return (i);
+	return (i - x);
 }
 
 void	test(int one, int two)
@@ -184,21 +191,35 @@ void	test(int one, int two)
 		printf(" ✅\n");
 	else
 	{
-		printf(" ❌\n");
-		printf("Expected: %d, got: %d\n", one, two);
+		printf("\n");
+		//printf("Expected: %d, got: %d\n", one, two);
 	}
-	printf("------------\n");
+	printf("------------------------------\n");
 }
 
-int		main(int ac, char const **argv)
+int		main(void)
 {
-	char str[] = "ZOB";
-	test(printf("'%i'", 42), ft_printf("'%d'", 42));
+	char *str = "test";
+	char **ptr = &str;
+
+	test(printf("'%d'", 4), ft_printf("'%d'", 4));
+	test(printf("'%d'", 42), ft_printf("'%d'", 42));
+	test(printf("'%d'", 420), ft_printf("'%d'", 420));
+	test(printf("'%i'", 4), ft_printf("'%i'", 4));
+	test(printf("'%i'", 42), ft_printf("'%i'", 42));
 	test(printf("'%i'", 420), ft_printf("'%i'", 420));
 	test(printf("'%c'", '!'), ft_printf("'%c'", '!'));
-	test(printf("'%s'", str),	ft_printf("'%s'", str));
+	test(printf("'%c, %c'", '!', '?'), ft_printf("'%c, %c'", '!', '?'));
+	test(printf("'%s'", "I"),	ft_printf("'%s'", "I"));
+	test(printf("'%s'", "Im"),	ft_printf("'%s'", "Im"));
+	test(printf("I"), ft_printf("I"));
 	test(printf("'Im a str'"), ft_printf("'Im a str'"));
+	test(printf("'%x'", 4),	ft_printf("'%x'", 4));
 	test(printf("'%x'", 42),	ft_printf("'%x'", 42));
+	test(printf("'%x'", 420),	ft_printf("'%x'", 420));
+	test(printf("'%X'", 4), ft_printf("'%X'", 4));
 	test(printf("'%X'", 42), ft_printf("'%X'", 42));
+	test(printf("'%X'", 420), ft_printf("'%X'", 420));
+	test(printf("'%p'", ptr), ft_printf("'%p'", ptr));
 	return (0);
 }
